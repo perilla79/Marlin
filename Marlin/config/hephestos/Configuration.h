a5f9af676f059aaef43c83ae0295863f8e37ef84
perilla79 <43704923+perilla79@users.noreply.github.com>
2018-09-29 19:06:30 +0300

    Update Configuration.h

--- Marlin/config/hephestos/Configuration.h
+++ Marlin/config/hephestos/Configuration.h
@@ -474,7 +474,7 @@ const bool Z_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the logic 
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 4000,100.47095761381482}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 400,100.47095761381482}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_ACCELERATION      {1100,1100,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
 #ifndef PREFER_MAX_SPEED
